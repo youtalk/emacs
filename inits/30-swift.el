eeaa3dc8855d2b5c38834e74d5c3815890b60659
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2020-10-02 16:14:39 +0900

    add swift

---
+++ inits/30-swift.el
@@ -0,0 +1,6 @@
+(use-package swift-mode)
+
+(use-package lsp-sourcekit
+  :after lsp-mode
+  :config
+  (setq lsp-sourcekit-executable "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp"))
