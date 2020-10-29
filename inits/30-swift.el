63b86bc71e063f4eafa7afb07b455b0421190f5c
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2020-10-29 17:47:14 +0900

    enable swift lsp

--- inits/30-swift.el
+++ inits/30-swift.el
@@ -1,6 +1,9 @@
-(use-package swift-mode)
+(use-package swift-mode
+  :hook
+  (swift-mode . (lambda () (lsp))))
 
 (use-package lsp-sourcekit
-  :after lsp-mode
+  :after
+  lsp-mode
   :config
   (setq lsp-sourcekit-executable "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp"))
