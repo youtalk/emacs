77c3e3e19d9faf209f9b72de36064075a1082eee
Yutaka Kondo <youtalk@pfrobotics.jp>
2022-12-09 17:23:08 +0900

    change typescript indent

--- inits/30-typescript.el
+++ inits/30-typescript.el
@@ -1,4 +1,9 @@
 (use-package lsp-mode
   :ensure t
   :commands (lsp lsp-deferred)
-  :hook (js-mode . lsp-deferred))
+  :hook (js-mode . (lambda ()
+                     (lsp-deferred)
+                     (setq js-indent-level 2)
+                     (setq typescript-indent-level 2)
+                     (setq tab-width 2)
+                     )))
