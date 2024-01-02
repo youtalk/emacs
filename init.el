27dd5b3d87d34629e8e9d483884f92f910437745
Yutaka Kondo <yutaka.kondo@youtalk.jp>
2024-01-02 12:47:04 +0900

    enable copilot

--- init.el
+++ init.el
@@ -201,3 +201,9 @@
                      (setq typescript-indent-level 2)
                      (setq tab-width 2)
                      )))
+(use-package copilot
+  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :ensure t)
+(add-hook 'prog-mode-hook 'copilot-mode)
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
